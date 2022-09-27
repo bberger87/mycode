a9e6b8cd32fc4ad6d031f4e0617e7f62ce77da00
bberger87 <bberger87@hotmail.com>
2022-09-27 15:58:33 +0000

    join and split

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main():
+    # create a list called list1
+    list = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak","Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    # display list1
+    print(list)
+
+main()
