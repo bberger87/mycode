97978995bf6c95aaf700cbb9693c354eee31e108
bberger87 <bberger87@hotmail.com>
2022-09-28 20:37:15 +0000

    learning about input function

--- numberguess.py
+++ numberguess.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
 
-num = 57
-found =0
-count =0
+import random
 
-while found==0:
-    guess = int(input("What's the magic numer? "))
-    count += 1
-    if num == guess:
-        print("\nYour guess is correct the correct numer")
-        found = 1
-        break:
-    elif guess in range(num-5,num):
-        print("\nVery close but too low")
-    elif num > guess:
-        print("\nToo low")
-    elif guess in range(num+1,num+5):
-        print("\nVery close but too high")
-    elif num < guess:
-        print("\nToo high")
-    print("\nGuess again")
 
-print("\nTHank you")
-print(f"Took you {count} guesses to find the correct number")
+def main():
+    print("\n\nWelcome to The Random Number Guessing Game")
+    print("\nA Random number gets generated between 1 and 100")
+    print("and the user gets to guess until the correct number is chosen")
+    num =  random.randint(1,100)
+    count =0
+
+    while True:
+        guess = int(input("\nWhat is your guess for the number  ? "))
+        count += 1
+        if num == guess:
+            print(f"\nCongratulations!!\n\nYour guess, {num}, is the correct number")
+            print(f"\nIt took {count} guess(es) to find the correct number")
+            break
+        elif guess in range(num-5,num):
+            print(f"\n{guess} is VERY close but LOW")
+        elif num > guess:
+            print(f"\n{guess} is TOO LOW")
+        elif guess in range(num+1,num+5):
+            print(f"\n{guess} is VERY close but HIGH")
+        elif num < guess:
+            print(f"\n{guess} is TOO HIGH")
+        print("\nGuess again")
+
+    print("\nThank you")
+    print("To play again rerun the script again\n")
+
+
+main()
